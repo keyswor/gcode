0fde260496e1a924c49c497b5ef3d0ecbb807fe9
keyswor <keyswor1@gmail.com>
2016-03-29 15:42:41 +0545

    kukl index

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;" />
+<title>KHANEPANI SCHEDULE| www.khanepani.com</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+</head>
+
+<body>
+    <div id="page">
+
+<div id="back">
+
+<div id="header">
+
+<div id="header_left">
+<h1>Kathmandu Drinking <span class="red">Water Schedule</span></h1>
+<h2>ghar ghar ma pani</h2>
+</div>
+
+<div id="header_right">
+
+
+  </div>
+  
+</div>
+
+<div id="left">
+
+<h4><span class="menu_first_letter">Navigation</span></h4>
+
+<div id="navcontainer">
+<ul id="navlist">
+<li id="active"><a href="#" id="current">Home</a></li>
+<li><a href="#">Download Schedule</a></li>
+<li><a href="#">Community</a></li>
+<li><a href="#">About us </a></li>
+</ul>
+</div>
+
+<h4>Contact us </h4>
+
+
+  <form id="form2" method="post" class="contact_us" action="">
+    <p><label>Name
+    <input type="text" class="fields_contact_us" name="textfield" />
+    </label>
+    <label>E-mail
+    <input type="text" class="fields_contact_us" name="textfield2" />
+	</label>
+	<label>
+    Your message:
+    <textarea name="textarea" cols="" rows=""></textarea>
+	</label>
+    <label>
+    <input type="submit" class="submit_button_contact" name="Submit3" value="Submit" />
+    </label></p>
+  </form>
+
+<h4>Suggested links </h4>
+
+
+
+<a href="http://www.facebook.com">Facebook</a></div>
+
+
+
+
+    <!-- slider -->
+    <DIV id='slider'>
+    <?php include('index-howto.php'); ?>
+   </DIV>
+    <!--Slider -->
+    <!-- schedule -->
+    <DIV ID='schedule'>
+     <?php include('schedulr.php'); ?>
+    </DIV>
+    <!--Schedule -->    
+            
+            
+         
+</div
+</body>
+</html>
